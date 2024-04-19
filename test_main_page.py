832c94ed71c2b62613981a32360d566ecefbed44
what-is-matrix <serg.glazkov@list.ru>
2024-04-19 15:30:16 +0300

    Rewrote main page tests using Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -9,5 +9,7 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
