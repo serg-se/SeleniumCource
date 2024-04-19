b73050f204725d6ee2a3f5303b19414c674c3df4
what-is-matrix <serg.glazkov@list.ru>
2024-04-19 15:54:28 +0300

    Extracted main page selectors to locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,8 @@
-from selenium.webdriver.common.by import By
-
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK, "#login_link")
         login_link.click()
