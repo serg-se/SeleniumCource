53cda4995d8848d584b4c76b38548ffc97fd4f4a
what-is-matrix <serg.glazkov@list.ru>
2024-04-19 20:03:57 +0300

    Added login link test for product page

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,13 @@ import pytest
 from pages.product_page import ProductPage
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
 @pytest.mark.parametrize(
     "link",
     [
